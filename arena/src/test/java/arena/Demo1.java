dfbc745174e79eba34d12bc42db7d7e94fe1e9b6
Aman9319 <Pushpendrasingh0193@gmail.com>
2019-05-08 14:30:34 +0530

    second

---
+++ arena/src/test/java/arena/Demo1.java
@@ -0,0 +1,14 @@
+package arena;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo1
+{
+@Test
+public void run()
+{
+Reporter.log("hiii", true);
+System.out.println("bye");
+}
+}
